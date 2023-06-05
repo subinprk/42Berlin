741e398d24e23be1bb4e2f64e11f102b387f0526
Subin Park <subpark@c4a7c6.42berlin.de>
2023-06-05 11:48:38 +0200

    I passed 2nd May this project!!!!! Finally!!!!

--- libft1/ft_lstmap_bonus.c
+++ libft1/ft_lstmap_bonus.c
@@ -6,31 +6,31 @@
 /*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 17:08:30 by subpark           #+#    #+#             */
-/*   Updated: 2023/06/01 17:34:48 by subpark          ###   ########.fr       */
+/*   Updated: 2023/06/02 11:02:44 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    t_list  *new;
-    t_list  *node;
+	t_list	*new;
+	t_list	*node;
 
-    node = NULL;
-    if (lst == NULL || f == NULL || del == NULL)
-        return (NULL);
-    while (lst)
-    {
-        new = ft_lstnew(f(lst->content));
-        if (!new)
-        {
-            ft_lstclear(&node, del);
-            return (NULL);
-        }
-        ft_lstadd_back(&node, new);
-        lst = lst->next;
-    }
-    new = NULL;
-    return (node);
+	node = NULL;
+	if (lst == NULL || f == NULL || del == NULL)
+		return (NULL);
+	while (lst)
+	{
+		new = ft_lstnew(f(lst->content));
+		if (!new)
+		{
+			ft_lstclear(&node, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&node, new);
+		lst = lst->next;
+	}
+	new = NULL;
+	return (node);
 }
