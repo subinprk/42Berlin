184fc2d372fcebbf921fada17f7cfc12481ec19f
Subin Park <subpark@c1b4c1.42berlin.de>
2023-09-01 18:10:47 +0200

    have to deal the merging from biggest, smallest part

--- new_push_swap/source/main.c
+++ new_push_swap/source/main.c
@@ -6,7 +6,7 @@
 /*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 18:10:37 by subpark           #+#    #+#             */
-/*   Updated: 2023/09/01 13:23:51 by subpark          ###   ########.fr       */
+/*   Updated: 2023/09/01 17:49:19 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,17 +35,21 @@ int	load_stack_one(char *str, t_list **lst)
 {
 	int *number;
 	int	i;
+	int	j;
 	char	**splited;
 	t_list	*new;
 
 	splited = ft_split(str, ' ');
 	i = 0;
 	while (splited[i])
+		i ++;
+	j = i;
+	while (0 < j)
 	{
 		number = (int *)malloc(sizeof(int));
 		if (!number)
 			return (-1);
-		*number = ft_atoi(splited[i]);
+		*number = ft_atoi(splited[j - 1]);
 		if (!(*number))
 			return (0);
 		new = ft_lstnew(number);
@@ -53,8 +57,12 @@ int	load_stack_one(char *str, t_list **lst)
 			*lst = new;
 		else
 			ft_lstadd_front(lst, new);
-		i ++;
+		j --;
 	}
+	j = 0;
+	while (splited[j])
+		free(splited[j ++]);
+	free(splited);
 	return (i + 1);
 }
 
@@ -105,8 +113,8 @@ int loadStackFromArgs(int argc, char **argv, t_list **stack_a)
 	}
 	else
 	{
-		i = 1;
-		while (i < argc)
+		i = argc;
+		while (0 < i)
 		{
 			if (!load_stack(argv[i], stack_a))
 			{
@@ -115,7 +123,7 @@ int loadStackFromArgs(int argc, char **argv, t_list **stack_a)
 			}
 			i++;
 		}
-		return argc - 1; // Return the number of elements loaded into the stack
+		return (argc - 1);
 	}
 }
 
@@ -125,8 +133,9 @@ int main(int argc, char **argv)
 	int j;
 	t_list *stack_a;
 	t_list *stack_b;
-	t_list *curr;
+		t_list *curr;
 	t_list *next;
+
 
 	int loadResult = loadStackFromArgs(argc, argv, &stack_a);
 	stack_b = NULL;
@@ -139,28 +148,28 @@ int main(int argc, char **argv)
 		ft_printf("Error\n");
 		return 1;
 	}
-
 	j = 0;
 	while (j < loadResult - 4)
 	{
 		pb(&stack_b, &stack_a);
 		j++;
 	}
-
 	make_order_3(&stack_a);
+	/*		curr = stack_a;
+	while (curr)
+	{
+		ft_printf("b4 order3: %d\n", *(int *)curr->content);
+		next = curr->next;
+		curr = next;
+	}*/
 	select_sort(&stack_a, &stack_b);
-
-	curr = stack_a;
+	/*	curr = stack_a;
 	while (curr)
 	{
 		ft_printf("stack a: %d\n", *(int *)curr->content);
 		next = curr->next;
 		curr = next;
-	}
-
+	}*/
 	ft_lstclear(&stack_a, free);
 	return 0;
 }
-
-
-
--- new_push_swap/source/select_sort.c
+++ new_push_swap/source/select_sort.c
@@ -6,7 +6,7 @@
 /*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 00:02:35 by siun              #+#    #+#             */
-/*   Updated: 2023/09/01 13:24:08 by subpark          ###   ########.fr       */
+/*   Updated: 2023/09/01 18:10:13 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ t_list *find_smallest(t_list *a)
 	while (curr)
 	{
 		if(*(int *)curr->content < *(int *)small->content)
-		small = curr;
+			small = curr;
 		curr = curr->next;
 	}
 	return (small);
@@ -46,48 +46,24 @@ t_list *find_smallest(t_list *a)
 
 void	select_sort(t_list **a, t_list **b)
 {
-	static int count;
-	int	tmp;
-	t_list	*curr;
-	//static t_list	*start;
-
 	if (b == NULL)
 		return ;
-	//curr = (*a);
-	//start = (*a);
-	/*//ft_printf("curr : %d\n", *(int*)curr->content);
-	if (*(int *)curr->content > *(int *)(*b)->content)
-	while (curr != NULL && *(int *)curr->content < *(int *)(*b)->content)
-	{
-	//	ft_printf("I'm in this number now: %d\n", *(int*)curr->content);
-		curr = curr->next;
-		count ++;
-	}
-	//ft_printf("count : %d\n", count);
-	tmp = 0;
-	while (tmp < count)
-	{
-		ra(a);
-		tmp ++;
-	}
-	pa(a, b);
-	while (count > 0)
-	{
-		rra(a);
-		count --;
-	}*/
-	if ((*b) != NULL && *(int *)find_biggest(*a)->content < *(int *)(*b)->content)
-	{
-		while (*a != find_biggest(*a))
-			ra(a);
-		pa(a, b);
-	}
-	else if ((*b) != NULL && *(int *)find_smallest(*a)->content > *(int *)(*b)->content)
+	if ((*b) != NULL && *(int *)find_biggest(*a)->content < *(int *)(find_smallest(*b))->content)
 	{
 		while (*a != find_smallest(*a))
 			rra(a);
 		pa(a, b);
 	}
+	if ((*b) != NULL && *(int *)find_biggest(*a)->content < *(int *)(*b)->content)
+		rb(b);
+	else if ((*b) != NULL && *(int *)(find_biggest(*b))->content < *(int *)(find_smallest(*a))->content)
+	{
+		while (*a != find_smallest(*a))
+			rra(a);
+		pa(a, b);
+	}
+	else if ((*b) != NULL && *(int *)find_smallest(*a)->content > *(int *)(*b)->content)
+			rb(b);
 	else if ((*b) != NULL && *(int *)(*a)->content < *(int *)(*b)->content)//have to fix here, it doesn't cover the case b->content is biggist
 			ra(a);
 	else if ((*b) != NULL && *(int *)(*a)->content > *(int *)(*b)->content && *(int *)ft_lstlast((*a))->content > *(int *)(*b)->content)
